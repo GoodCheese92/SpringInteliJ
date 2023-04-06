53be8f73e9c2f9b3fb89f65d8c4f9a96dcaf8a8e
GoodCheese92 <dltjdwls555@gmail.com>
2023-04-06 18:01:14 +0900

    2023-04-06 집가서 하기..

--- InteliJSpring/Ex_0406_SpringVisit/src/main/java/dao/VisitDAO.java
+++ InteliJSpring/Ex_0406_SpringVisit/src/main/java/dao/VisitDAO.java
@@ -8,16 +8,13 @@ import java.util.List;
 public class VisitDAO {
     SqlSession sqlSession;
 
-    public void setSqlSession(SqlSession sqlSession) {
+    public VisitDAO(SqlSession sqlSession) {
         this.sqlSession = sqlSession;
-    }
-
-    public VisitDAO() {
-        System.out.println("dao 생성자");
     } // end of constructor
 
     public List<VisitVO> selectList(){
-        List<VisitVO> visit_list = sqlSession.selectList("v.visit_list");
-        return visit_list;
-    } // end of class
+        List<VisitVO> list = sqlSession.selectList("v.visit_list");
+        return list;
+    } // end of selectList()
+
 } // end of class
